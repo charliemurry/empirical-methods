b5a89fb3e25a92d0d648843ee62cd6acc1f067e9
Charlie Murry <murry.econ@gmail.com>
2021-03-25 11:42:02 -0400

    mcmc code update

--- Part 7 - MCMC/code_MCMC/logLike.m
+++ Part 7 - MCMC/code_MCMC/logLike.m
@@ -1,9 +1,9 @@
 %This function computes the log-likelihood of the data for theta=propose
 % for use with main_MCMC2.m
-function f_out = logLike(propose)
+function f_out = logLike(propose, data)
 %load data_n24;
 %data = data_n24;
-global data;
+% global data;
 mu_a  = propose(1);
 sig_a = propose(2);
 ff = log(normpdf(data,mu_a,sig_a));
--- Part 7 - MCMC/code_MCMC/metroHastMain.m
+++ Part 7 - MCMC/code_MCMC/metroHastMain.m
@@ -14,7 +14,7 @@ clear all;
 % save data_n24;
 % f_out = log(mvnpdf(data_n24,mu_a,var_a));
 
-global data;
+% global data;
 load data_n24;
 data = data_n24;
 
@@ -35,6 +35,10 @@ prosig      = 0.1*eye(2);
 
 theta0 = 5*ones(1,2);
 
+objfnc = @(x) logLike(x,data)
+objfncSA = @(x) -logLike(x,data)
+
+
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                    Construct the Markov chain
@@ -44,7 +48,7 @@ tic;
 T          = 20000;            % total length of the chain
 theta      = zeros(T,2);
 theta(1,:) = theta0;
-curr_pi    = logLike(theta0) + log(mvnpdf(theta0,prmu,prsig));
+curr_pi    = objfnc(theta0) + log(mvnpdf(theta0,prmu,prsig));
 
 iaccept    = 0;               % counter for number of acceptances
 t = 2;
@@ -52,7 +56,7 @@ while t <= T
    innov = promu' + normrnd([0 0], [1 1])*prosig;
    propose   = theta(t-1,:) + innov;    
    propose(2)= abs(propose(2));
-   lik       = logLike(propose) ;                 % log-likelihood at proposed theta
+   lik       = objfnc(propose) ;                 % log-likelihood at proposed theta
    prior     = log(mvnpdf(propose,prmu,prsig)) ; % log of prior at proposed theta 
    prop_pi   = lik + prior;
    delta     = prop_pi - curr_pi ;   %Log-ratio of proposal to current
@@ -99,7 +103,7 @@ tic;
 start = [1 1];
 
 %Anonymous function for the log posterior...
-l_post = @(th) logLike(th) + log(mvnpdf(th, prmu, prsig));
+l_post = @(th) logLike(th, data) + log(mvnpdf(th, prmu, prsig));
 
 %Don't need this since it is symmetric...cancels out...
 %l_proppdf = @(x,y) log(mvnpdf((x-y), promu', prosig));
@@ -116,4 +120,13 @@ v_theta = var(t_samp(B:end, :))
 acc
 toc;
 
+
+%% 
+fprintf(1,'Simulated Annealing algorithm on same likelihood fnc\n');
+
+rng default
+options = optimoptions('simulannealbnd','PlotFcns',...
+          {@saplotbestx,@saplotbestf,@saplotx,@saplotf});
+xSA = simulannealbnd(objfncSA,theta0,[],[],options);
     
+
